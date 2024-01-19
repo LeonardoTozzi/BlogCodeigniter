37a5c0b5b46ac72d92f8fcb9c4753786c951eb15
LeonardoTozzi <leonardotozzis@gmail.com>
2024-01-19 11:54:07 -0300

    First Commit

---
+++ app/Controllers/Search.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+
+class Search extends BaseController
+{
+    public function index()
+    {
+        var_dump($this->request->getGet('s'));
+    }
+}
